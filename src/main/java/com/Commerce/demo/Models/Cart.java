4a826ac441ec2c3170874250b79231e4d0be7025
YigitAgr <yigit.agar22@gmail.com>
2024-10-10 22:53:40 +0300

    Entity relationships added.

--- src/main/java/com/Commerce/demo/Models/Cart.java
+++ src/main/java/com/Commerce/demo/Models/Cart.java
@@ -1,4 +1,20 @@
 package com.Commerce.demo.Models;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "Carts")
 public class Cart extends Base{
+
+
+    //One cart can have multiple orders . One cart has one customer
+    @OneToOne
+    @JoinColumn(name = "customer_id",unique = true)
+    private Customer customer;
+
+
+    @OneToMany(mappedBy = "cart" ,cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+    private List<Order> orders;
 }
--- src/main/java/com/Commerce/demo/Models/Customer.java
+++ src/main/java/com/Commerce/demo/Models/Customer.java
@@ -1,4 +1,17 @@
 package com.Commerce.demo.Models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "Customers")
 public class Customer extends Base{
+
+    private String customerName;
+    private String customerPassword;
+
+    @OneToOne(mappedBy = "customer",cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Cart cart;
+
 }
