2350e044ca9a882c9c53bfc39d4bca48ba11d493
YigitAgr <yigit.agar22@gmail.com>
2024-10-10 12:35:45 +0300

    Service and repostiory file added.

---
+++ src/main/java/com/Commerce/demo/Repositorys/CartRepository.java
@@ -0,0 +1,7 @@
+package com.Commerce.demo.Repositorys;
+
+import com.Commerce.demo.Models.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart, Integer> {
+}
